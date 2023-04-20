ff9fdd3bffcd14500a598dcf901cb18fd239f329
TheK321 <thek321xd@gmail.com>
2023-04-19 22:35:48 -0600

    Updated to get only people with birthdays today

--- Reto3/correoCumple.py
+++ Reto3/correoCumple.py
@@ -1,10 +1,21 @@
+import pandas as pd
 from TarjetaDeCumple import TarjetaDeCumpleanios
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
-# Crear una instancia de la clase TarjetaDeCumpleaños
-tarjeta = TarjetaDeCumpleanios('Juan', 'Mario', '¡Feliz cumpleaños! Que tengas un gran día.')
+# Obtener la fecha actual
+fecha_actual = datetime.now().date()
+# Leer el archivo CSV utilizando pandas
+df = pd.read_csv('./Reto3/correos.csv')
 
-# Llamar al método generar_html
-html = tarjeta.generar_html()
+# Filtrar solo las personas que cumplen años en la fecha actual
+cumplen_anios_hoy = df[df.apply(lambda x: (int(x['mm']), int(x['dd'])) == (fecha_actual.month, fecha_actual.day), axis=1)]
 
-# Imprimir html en consola (prueba)
-print(html)
+# Obtener la edad de cada persona que cumple años hoy
+for index, row in cumplen_anios_hoy.iterrows():
+    nombre = row['nombre']
+    correo_electronico = row['email']
+    fecha_nacimiento = datetime(int(row['aa']), int(row['mm']), int(row['dd'])).date()
+    edad = relativedelta(fecha_actual, fecha_nacimiento).years
+    html = TarjetaDeCumpleanios({nombre}, 'Mario', '¡Felices {edad}! Que tengas un gran día y cumplas muchos más.').generar_html()
+    print(html)
