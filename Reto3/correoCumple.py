ec925f683c6e1d346db9bdf3ef586d9038c63775
TheK321 <thek321xd@gmail.com>
2023-04-19 20:43:47 -0600

    Added TarjetaDeCumpleanios class and test file

---
+++ Reto3/correoCumple.py
@@ -0,0 +1,10 @@
+from TarjetaDeCumple import TarjetaDeCumpleanios
+
+# Crear una instancia de la clase TarjetaDeCumpleaños
+tarjeta = TarjetaDeCumpleanios('Juan', 'Mario', '¡Feliz cumpleaños! Que tengas un gran día.')
+
+# Llamar al método generar_html
+html = tarjeta.generar_html()
+
+# Imprimir html en consola (prueba)
+print(html)
