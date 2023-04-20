a7678adb80b42b6ee23bff869b688f7d87c55a48
TheK321 <thek321xd@gmail.com>
2023-04-20 08:01:18 -0600

    Added bash-based task automation daily

---
+++ Reto3/schedulebdays.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Ruta al directorio del script de Python
+SCRIPT_DIR=/home/thek321/Reto3/
+
+# Nombre del script de Python
+SCRIPT_NAME=correoCumple.py
+
+# Contenido del script de shell que ejecuta el script de Python
+SCRIPT_CONTENT="#!/bin/bash\npython3 ${SCRIPT_DIR}${SCRIPT_NAME}"
+
+# Guarda el script de shell en un archivo
+echo -e "${SCRIPT_CONTENT}" > "${SCRIPT_DIR}ejecutar_bdays.sh"
+
+# Da permisos de ejecución al script de shell
+chmod +x "${SCRIPT_DIR}ejecutar_bdays.sh"
+
+# Configura cron para que ejecute el script de shell cada lunes a la medianoche
+(crontab -l 2>/dev/null; echo "0 8 * * * ${SCRIPT_DIR}ejecutar_bdays.sh") | crontab -
+
+echo "Se enviarán las felicitaciones diariamente"
