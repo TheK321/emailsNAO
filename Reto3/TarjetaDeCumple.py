32c28c41a32ee1c19a5927d7d6980c6a832fe89f
TheK321 <thek321xd@gmail.com>
2023-04-20 00:26:44 -0600

    Added sending email functionality

--- Reto3/TarjetaDeCumple.py
+++ Reto3/TarjetaDeCumple.py
@@ -3,48 +3,26 @@ class TarjetaDeCumpleanios:
         self.destinatario = destinatario
         self.remitente = remitente
         self.mensaje = mensaje
-        
+
     def generar_html(self):
         html = '''
+        <!DOCTYPE html>
         <html>
-            <head>
-                <title>Tarjeta de Cumpleaños</title>
-                <style>
-                    body {{
-                        background-color: #FDC8C3;
-                        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-                        text-align: center;
-                    }}
-                    h1 {{
-                        font-size: 48px;
-                        margin-top: 80px;
-                        margin-bottom: 0;
-                    }}
-                    h2 {{
-                        font-size: 36px;
-                        margin-top: 20px;
-                        margin-bottom: 0;
-                    }}
-                    p {{
-                        font-size: 24px;
-                        margin-top: 10px;
-                        margin-bottom: 10px;
-                    }}
-                    img {{
-                        width: 400px;
-                        height: 400px;
-                        margin-top: 50px;
-                    }}
-                </style>
-            </head>
-            <body>
-                <h1>¡Feliz Cumpleaños, {0}!</h1>
-                <h2>De: {1}</h2>
-                <h2>Para: {0}</h2>
-                <hr>
-                <p>{2}</p>
-                <img src="https://example.com/tarjeta_de_cumpleanos.png" alt="Tarjeta de Cumpleaños">
-            </body>
+        <head>
+            <meta charset="UTF-8">
+            <title>Tarjeta de Cumpleaños</title>
+        </head>
+        <body style="background-color: #FDC8C3;
+                    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                    text-align: center;">
+                    <h1>¡Feliz Cumpleaños, {0}!</h1>
+                    <h2>De: {1}</h2>
+                    <h2>Para: {0}</h2>
+                    <p style="font-size: 24px;
+                    margin-top: 10px;
+                    margin-bottom: 10px;">{2}</p>
+                    <img src="https://www.imageshine.in/uploads/gallery/Colorful_happy_birthday_lettering.png" alt="Tarjeta de Cumpleaños">
+        </body>
         </html>
         '''.format(self.destinatario, self.remitente, self.mensaje)
         return html
--- Reto3/correoCumple.py
+++ Reto3/correoCumple.py
@@ -1,15 +1,20 @@
 import pandas as pd
+import yagmail
 from TarjetaDeCumple import TarjetaDeCumpleanios
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 # Obtener la fecha actual
 fecha_actual = datetime.now().date()
+
 # Leer el archivo CSV utilizando pandas
 df = pd.read_csv('./Reto3/correos.csv')
 
 # Filtrar solo las personas que cumplen años en la fecha actual
 cumplen_anios_hoy = df[df.apply(lambda x: (int(x['mm']), int(x['dd'])) == (fecha_actual.month, fecha_actual.day), axis=1)]
+
+#Definir datos de envío
+yag = yagmail.SMTP("thek321xd@gmail.com", oauth2_file="~/oauth2_creds.json")
 
 # Obtener la edad de cada persona que cumple años hoy
 for index, row in cumplen_anios_hoy.iterrows():
@@ -17,5 +22,7 @@ for index, row in cumplen_anios_hoy.iterrows():
     correo_electronico = row['email']
     fecha_nacimiento = datetime(int(row['aa']), int(row['mm']), int(row['dd'])).date()
     edad = relativedelta(fecha_actual, fecha_nacimiento).years
-    html = TarjetaDeCumpleanios({nombre}, 'Mario', '¡Felices {edad}! Que tengas un gran día y cumplas muchos más.').generar_html()
-    print(html)
+    html = TarjetaDeCumpleanios(nombre, 'Mario', f'¡Felices {edad}! Que tengas un gran día y cumplas muchos más.').generar_html()
+    to = row['email']
+    subject = f"Feliz cumpleaños, {nombre}"
+    yag.send(to=to, subject=subject, contents=html)
