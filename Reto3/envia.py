9f57fe792c9a910326434aa96e2aca5e7879731e
TheK321 <thek321xd@gmail.com>
2023-04-19 22:09:24 -0600

    Changed csv structure to include birthdate

--- Reto3/envia.py
+++ Reto3/envia.py
@@ -6,8 +6,8 @@ yag = yagmail.SMTP("thek321xd@gmail.com", oauth2_file="~/oauth2_creds.json")
 with open('/home/thek321/Reto3/correos.csv') as archivocsv:
     reader = csv.DictReader(archivocsv)
     for row in reader:
-        alias = row['Alias']
-        to = row['Email']
+        alias = row['nombre']
+        to = row['email']
         subject = f"Hola, {alias}, desde Python"
         contents = '¡Este es un correo de prueba enviado desde Python!'
         yag.send(to=to, subject=subject, contents=contents)
