b9c52e08381c19ffeaf6b9415b04d3c64a76fbc7
TheK321 <thek321xd@gmail.com>
2023-04-24 00:05:02 -0600

    Funcional con OAuth

---
+++ Reto3/ejecutar_script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 /home/thek321/Reto3/envia.py
---
+++ Reto3/envia.py
@@ -0,0 +1,13 @@
+import csv
+import yagmail
+
+yag = yagmail.SMTP("thek321xd@gmail.com", oauth2_file="~/oauth2_creds.json")
+
+with open('/home/thek321/Reto3/correos.csv') as archivocsv:
+    reader = csv.DictReader(archivocsv)
+    for row in reader:
+        alias = row['Alias']
+        to = row['Email']
+        subject = f"Hola, {alias}, desde Python"
+        contents = '¡Este es un correo de prueba enviado desde Python!'
+        yag.send(to=to, subject=subject, contents=contents)
